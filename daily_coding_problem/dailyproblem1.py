a27edd31bd6bc345ce6e560eb3643a50db256e77
Eliana Abraham <elabraha@umich.edu>
2018-11-25 23:53:12 -0500

    adding daily coding problem folder

---
+++ daily_coding_problem/dailyproblem1.py
@@ -0,0 +1,16 @@
+# Given a list of numbers and a number k, return whether any two numbers from
+# the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 id 17
+# Bonus: Can you do this in one pass?
+
+def add_to_k(nums, k):
+    diffs = {}
+    for n in nums:
+        diff = k - n
+
+def main():
+    # call my function
+    nums = [10, 15, 3, 7]
+    k = 17
+    add_t_k(nums, k)
+main()
