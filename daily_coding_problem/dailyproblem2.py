b59086b01ecabd0ef8ead00a7ce25e47d9b73646
Eliana Abraham <elabraha@umich.edu>
2018-11-26 10:47:32 -0500

    fixing solution 2 and adding solution 3

--- daily_coding_problem/dailyproblem2.py
+++ daily_coding_problem/dailyproblem2.py
@@ -35,26 +35,54 @@ def product_of_list(nums):
 def product_list_no_division(nums):
     # This one is o(n^2) please let me know if there is a better way to do this
     # without division
-    new_arr = [1 for _ in range(len(nums))]
-    for x, n in enumerate(nums):
-        for i in range(len(new_arr)):
-            if x!= i:
-                new_arr[i] *= n
+    # new_arr = [1 for _ in range(len(nums))]
+    # for x, n in enumerate(nums):
+    #     for i in range(len(new_arr)):
+    #         if x!= i:
+    #             new_arr[i] *= n
+    # return new_arr
+# TODO: Redo above with prefix suffix solution
+    # Now this solution is o(n)
+    prefix = [0 for _ in range(len(nums))]
+    suffix = [0 for _ in range(len(nums))]
+    prefix_product = 1
+    for i, n in enumerate(nums):
+        # print(prefix, prefix_product)
+        prefix[i] = prefix_product
+        prefix_product *= n
+    suffix_product = 1
+    i = len(nums) - 1
+    for n in reversed(nums):
+        # print (suffix, suffix_product)
+        suffix[i] = suffix_product
+        suffix_product *= nums[i]
+        i -= 1
+    new_arr = [0 for _ in range(len(nums))]
+    for index in range(len(nums)):
+        # print(new_arr)
+        if index == 0:
+            new_arr[index] = suffix[index]
+        elif index == len(nums) - 1:
+            new_arr[index] = prefix[index]
+        else:
+            new_arr[index] = prefix[index]*suffix[index]
     return new_arr
 
 def main():
     nums = [1, 2, 3, 4, 5]
     comp = [120, 60, 40, 30, 24]
-    nums = [3,2,1]
-    comp = [2,3,6]
-    nums = [0, 1, 2, 0]
-    comp = [0, 0, 0, 0]
+    # nums = [3,2,1]
+    # comp = [2,3,6]
+    # nums = [0, 1, 2, 0]
+    # comp = [0, 0, 0, 0]
     nums = [0, 1, 2, 3]
-    nums = [6, 0, 0, 0]
-    # if product_of_list(nums) == comp:
-    #     print("Correct!")
-    # else:
-    #     print("Incorrect.")
+    comp = [6, 0, 0, 0]
+    nums = [2, 1, 3, 0, 4]
+    comp = [0, 0, 0, 24, 0]
+    if product_of_list(nums) == comp:
+        print("Correct!")
+    else:
+        print("Incorrect.")
     if product_list_no_division(nums) == comp:
         print("Correct!")
     else:
