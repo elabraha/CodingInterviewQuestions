9f022e7fdb5fb8313ad55618f50f9e5231ac0fcb
Eliana Abraham <elabraha@umich.edu>
2018-11-27 00:18:51 -0500

    finishe solution 3

--- daily_coding_problem/dailyproblem3.py
+++ daily_coding_problem/dailyproblem3.py
@@ -13,7 +13,43 @@
 # node = Node('root', Node('left', Node('left.left')), Node('right'))
 # assert deserialize(serialize(node)).left.left.val == 'left.left'
 
+# I needed help wit this oneself.
+# Need to work on binary trees and recursion
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def tree_walk(node, str):
+    if node:
+        str.append(node.val)
+        tree_walk(node.left, str)
+        tree_walk(node.right, str)
+    else:
+        str.append("null")
+
+def serialize(node):
+    serial_str = []
+    tree_walk(node, serial_str)
+    serial_str = ', '.join(serial_str)
+    return serial_str
+
+def tree_build(serial_list):
+    if serial_list[0] == 'null':
+        serial_list.pop(0)
+        return None;
+    root = Node(serial_list.pop(0), tree_build(serial_list), tree_build(serial_list))
+    return root
+
+
+def deserialize(serial_str):
+    serial_list = serial_str.split(', ')
+    return tree_build(serial_list)
+
 def main():
-    pass
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+    if deserialize(serialize(node)).left.left.val == 'left.left':
+        print("Yayy!")
 
 main()
